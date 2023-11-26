1ca6cfacd85c0531b13e92227bea1b8f065b774e
arafathossainclass283 <152077224+arafathossainclass283@users.noreply.github.com>
2023-11-26 22:53:27 +0600

    Update [...postpath].tsx

--- pages/[...postpath].tsx
+++ pages/[...postpath].tsx
@@ -19,7 +19,7 @@ export const getServerSideProps: GetServerSideProps = async (ctx) => {
 			redirect: {
 				permanent: false,
 				destination: `${
-					`http://mindreferesher.local/` + encodeURI(path as string)
+					`https://mindrefereshera2z.blogspot.com/` + encodeURI(path as string)
 				}`,
 			},
 		};
