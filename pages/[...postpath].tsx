4322ada938dd8f82018044ed300dc4ba5961f1b0
arafathossainclass283 <152077224+arafathossainclass283@users.noreply.github.com>
2023-11-26 22:49:49 +0600

    Update [...postpath].tsx

--- pages/[...postpath].tsx
+++ pages/[...postpath].tsx
@@ -19,7 +19,7 @@ export const getServerSideProps: GetServerSideProps = async (ctx) => {
 			redirect: {
 				permanent: false,
 				destination: `${
-					`https://saveourstateok.org/` + encodeURI(path as string)
+					`http://mindreferesher.local/` + encodeURI(path as string)
 				}`,
 			},
 		};
